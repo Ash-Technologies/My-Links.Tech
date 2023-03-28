a63cdc73b53cbf2d4cc772b8235598cd2d42ac74
Aashutosh Soni <ashutoshsoni130@gmail.com>
2023-03-28 16:22:15 +0530

    created 400,404,500 codes

---
+++ responseCodes/404.php
@@ -0,0 +1,18 @@
+<?php echo $_SERVER['REQUEST_URI']; ?> does not exist, sorry.<br><br>
+which means you can acquire this username😁😁😉<br><br>
+Either user is or not registered or maybe there is some 🤏  problem...<br>
+Please contact Admin🙄<br><br>
+
+
+
+click here to go to  
+<a href='https://my-links.tech' target='_blank'>HomePage</a>
+or 
+<a href='https://my-links.tech/ContactUs' target='_blank'>Contact</a>
+<a href='https://ashutosh7i.me' target='_blank'>Admin😵</a>
+<br>
+    <a href="https://my-links.tech/privacy.html" style="color: grey">📜Privacy Policy📜</a>
+    <a href="https://my-links.tech/tos.html" style="color: grey">Terms Of Service📜</a>
+
+<br><br><br>
+<h2>🌋This is ERROR 404☠ </h2><br>
